465f801a88e82a0074aa9d0d010821b842922963
SBoyDeluxe <213826343+SBoyDeluxe@users.noreply.github.com>
2025-11-05 15:30:42 +0100

    ParticipantInputReducer update
    - Alike in structure to FeatureReducer :
        - Uses a top-level declared variable and switch-case to handle all actions
    -No validation done in reducer, validation and validation-errors are expected to be handled at UI-level (Checking the input for action.payload before calling dispatcher)
     - action.payload is expected to hold : userId and wanted add/remove-variable

--- src/modules/react/components/reducers/ParticipantInputReducer.tsx
+++ src/modules/react/components/reducers/ParticipantInputReducer.tsx
@@ -5,29 +5,34 @@ export type ParticipantInputData = {
 
     username: string,
     userType: string[],
-    userId : number
+    userId: number
 };
 export type ClientInputData = {
 
     username: string,
-    userId : number
+    userId: number
 };
 export function useParticipantReducer() {
 
-    let projectManagers: ParticipantInputData[] = [{ username: "", userType: [""] }];
-    let projectDevelopers: ParticipantInputData[] = [{ username: "", userType: [""], userId:-1 }];
-    let projectClients: ClientInputData[] = [{ username: "", userId:-1 }];
-     //The creating user is implicitly manager of the project 
+    const initState = getInitState();
+
+    return useReducer(ParticipantInputReducer, initState);
+}
+
+function getInitState() {
+    let projectManagers: ParticipantInputData[] = [{ username: "", userType: [""], userId: -1 }];
+    let projectDevelopers: ParticipantInputData[] = [{ username: "", userType: [""], userId: -1 }];
+    let projectClients: ClientInputData[] = [{ username: "", userId: -1 }];
+    //The creating user is implicitly manager of the project 
     const loggedInUser = UserStore.getSnapshotUser();
-    projectManagers[0] = { username: loggedInUser?.username.username!, userType: ["Creator"],userId: loggedInUser?.authParameters.userId!  };
+    projectManagers[0] = { username: loggedInUser?.username.username!, userType: ["Creator"], userId: loggedInUser?.authParameters.userId! };
 
     const initState = {
         projectManagers,
         projectDevelopers,
         projectClients
     };
-
-    return useReducer(ParticipantInputReducer, initState);
+    return initState;
 }
 
 function ParticipantInputReducer(participantsState: {
@@ -35,244 +40,261 @@ function ParticipantInputReducer(participantsState: {
     projectDevelopers: ParticipantInputData[],
     projectClients: ClientInputData[],
 }, action: { type: string, payload: any }) {
-    const projectDevelopersIsUninitiated = participantsState.projectDevelopers[0].userType[0] === "" && participantsState.projectDevelopers[0].username === "";
+
+    let returnState = participantsState;
+    const projectDevelopersIsUninitiated = participantsState.projectDevelopers[0].userId = -1;
+    const projectManagersIsUninitated = participantsState.projectManagers[0].userId = -1;
     let initClauseRan = false;
-    const projectClientsIsUninitiated = participantsState.projectClients[0].username === "";
-  
-    //Check so that we have values in our arrays
-    if ( (projectClientsIsUninitiated ) && ( action.type==="ADD_CLIENT_INPUT_DATA")) 
-        {
-   
-           
-            let clients =  [ action.payload]
-            
-            return ({...participantsState,
-                    projectClients: clients
-            });
-           
+    const projectClientsIsUninitiated = participantsState.projectClients[0].userId == -1;
     
-    } 
-    else if(projectDevelopersIsUninitiated && action.type==="ADD_DEVELOPER_INPUT_DATA" ){
 
-        let developerEntry;
-         if (action.payload.userType) {
-                developerEntry = [action.payload]
-            } else {
-                developerEntry = [{ username: action.payload.username, userType: [""], userId : action.payload.userId }]
+
+
+        switch (action.type) {
+            case "ADD_MANAGER_INPUT_DATA": {
+
+                if (projectManagersIsUninitated) {
+
+                    returnState = participantsState = {
+                        ...participantsState,
+                        projectManagers: [action.payload]
+                    };
+
+                } else {
+                    returnState = {
+                        ...participantsState,
+                        projectManagers: participantsState.projectManagers.concat({
+                            username: action.payload.username,
+                            userType: action.payload.userType,
+                            userId: action.payload.userId
+                        })
+                    };
+
+                }
+
+
+
+
 
             }
-            return ({
-                ...participantsState,
-                projectDevelopers:developerEntry
-            })
-    }
-    else{
-    
 
-
-    switch (action.type) {
-        case "ADD_MANAGER_INPUT_DATA": {
-
-             const userTypeVar =   (action.payload.userType) ? action.payload.userType : [""];
-            let newState = ({
-                ...participantsState,
-                projectManagers: participantsState.projectManagers.concat({username:action.payload.username, userType:userTypeVar, userId:action.payload.userId}),
-                
-
-            });
-
-            return newState;
+                break;
+            case "ADD_MANAGER_USER_TYPE": {
 
 
 
-        }
 
-            break;
-        case "ADD_MANAGER_USER_TYPE": {
-
-
-            const managerToAddTypeTo = participantsState.projectManagers.filter((manager, _index) => manager.username === action.payload.username)[0];
-            //We only want to run the rest if and only if a manager with the username is found
-            if (managerToAddTypeTo) {
-                const newUserTypeArray = managerToAddTypeTo.userType.concat(action.payload.userType);
-
-                return ({
+                returnState = {
                     ...participantsState,
-                   projectManagers: participantsState.projectManagers.filter((dev) => dev.username !== action.payload.username).concat({...managerToAddTypeTo,  userType: newUserTypeArray })
-                });
+                    projectManagers: participantsState.projectManagers.map((manager, index) => {
+                        let returnManager = manager;
+                        if (manager.userId == action.payload.userId) {
+                            const newManagerInstance: ParticipantInputData = {
+                                username: manager.username,
+                                userType: manager.userType.concat(action.payload.userType),
+                                userId: manager.userId
+                            };
+                            returnManager = newManagerInstance;
+                        }
+
+                        return returnManager;
+                    })
+                }
+
+
+
+
+
             }
-            else {
-                return participantsState;
-            }
+
+                break;
+            case "REMOVE_MANAGER_USER_TYPE": {
 
 
+                
+                    const newProjectManagerArray = participantsState.projectManagers.map((manager)=>{
+                        let returnManager = manager;
+                        if(manager.userId == action.payload.userId){
+                         returnManager =  { 
+                            userId:manager.userId,
+                            userType:manager.userType.filter((userType) => userType !== action.payload.userType),
+                            username:manager.username
+                        }}
+                        return returnManager;
+                    });
 
-        }
-
-            break;
-        case "REMOVE_MANAGER_USER_TYPE": {
-            
-           
-            const managerToRemoveTypeTo = participantsState.projectManagers.filter((manager, _index) => manager.username === action.payload.username)[0];
-            if(managerToRemoveTypeTo){
-            const newUserTypeArray = managerToRemoveTypeTo.userType.filter((userType) => userType !== action.payload.userType);
-
-            return ({
-                ...participantsState,
-               projectManagers:participantsState.projectManagers.filter((dev) => dev.username !== action.payload.username).concat({...managerToRemoveTypeTo, username: action.payload.username, userType: newUserTypeArray })
-            });}
-            else{
-                    return {   ...participantsState}
-            }
-     
-
-
-        }
-
-            break;
-        case "ADD_DEVELOPER_INPUT_DATA": {
-
-                   const userTypeVar =   (action.payload.userType) ? action.payload.userType : [""];
-
-            return ({
-                ...participantsState,
-                projectDevelopers: participantsState.projectDevelopers.concat({username:action.payload.username, userType:userTypeVar, userId:action.payload.userId })
-            });
-
-
-
-        }
-
-            break;
-        case "ADD_DEVELOPER_USER_TYPE": {
-
-
-            let developerToAddTypeTo = participantsState.projectDevelopers.filter((dev, _index) => dev.username === action.payload.username)[0];
-            if(developerToAddTypeTo){
-            const newUserTypeArray = developerToAddTypeTo.userType.concat(action.payload.userType);
-
-            return ({
-                ...participantsState,
-               projectDevelopers: participantsState.projectDevelopers.filter((dev) => dev.username !== action.payload.username).concat({ ...developerToAddTypeTo, userType:newUserTypeArray})
-            });
-
-        }else{
-
-            return participantsState;
-        }
-
-        }
-
-            break;
-        case "REMOVE_DEVELOPER_USER_TYPE": {
-
-            
-
-
-            let developerToRemoveTypeTo = participantsState.projectDevelopers.filter((dev, _index) => dev.username === action.payload.username)[0];
-            if(developerToRemoveTypeTo){
-            const newUserTypeArray = developerToRemoveTypeTo.userType.filter((userType) => userType !== action.payload.userType);
-
-            return ({
-                ...participantsState,
-                projectDevelopers: participantsState.projectDevelopers.filter((dev) => dev.username !== action.payload.username).concat({ ...developerToRemoveTypeTo, userType: newUserTypeArray })
-            });
-
-        } else{
-            return {...participantsState};
-        }
-
-        }
-
-            break;
-        case "ADD_CLIENT_INPUT_DATA": {
-
-
-
-            return ({
-                ...participantsState,
-                projectClients: participantsState.projectClients.concat(action.payload)
-            });
-
-
-
-        }
-
-            break;
-        case "REMOVE_MANAGER_INPUT_DATA": {
-
-            //A project must always have at least one manager
-            if(participantsState.projectManagers.length>1){
-            return ({
+                    returnState = ({
+                        ...participantsState,
+                        projectManagers:newProjectManagerArray
+                    }
+                    );
+                }
                
-                ...participantsState,
-                 projectManagers: participantsState.projectManagers.filter((manager) => (manager.username !== action.payload.username)),
 
-            });
-            }else{
-                return {...participantsState}
-            }
+
+
             
 
+                break;
+            case "ADD_DEVELOPER_INPUT_DATA": {
 
+                if(projectDevelopersIsUninitiated){
+                    returnState ={
+                        ...participantsState,
+                        projectDevelopers : [action.payload]
+                    }
+                }else{
+                     returnState ={
+                        ...participantsState,
+                        projectDevelopers : participantsState.projectManagers.concat(action.payload)
+                    }
+                }
+
+               
+
+
+
+            }
+
+                break;
+            case "ADD_DEVELOPER_USER_TYPE": {
+
+
+                let developerToAddTypeTo = participantsState.projectDevelopers.filter((dev, _index) => dev.username === action.payload.username)[0];
+                if (developerToAddTypeTo) {
+                    const newUserTypeArray = developerToAddTypeTo.userType.concat(action.payload.userType);
+
+                    returnState = ({
+                        ...participantsState,
+                        projectDevelopers: participantsState.projectDevelopers.filter((dev) => dev.username !== action.payload.username).concat({ ...developerToAddTypeTo, userType: newUserTypeArray })
+                    });
+
+                } else {
+
+                    returnState = participantsState;
+                }
+
+            }
+
+                break;
+            case "REMOVE_DEVELOPER_USER_TYPE": {
+
+
+
+
+                const newProjectDeveloperArray = participantsState.projectDevelopers.map((dev)=>{
+                        let returnDeveloper = dev;
+                        if(dev.userId == action.payload.userId){
+                         returnDeveloper =  { 
+                            userId:dev.userId,
+                            userType:dev.userType.filter((userType) => userType !== action.payload.userType),
+                            username:dev.username
+                        }}
+                        return returnDeveloper;
+                    });
+
+                    returnState = ({
+                        ...participantsState,
+                        projectDevelopers:newProjectDeveloperArray
+                    }
+                    );
+
+            }
+
+                break;
+            case "ADD_CLIENT_INPUT_DATA": {
+
+
+
+               
+                if(projectClientsIsUninitiated){
+                    returnState ={
+                        ...participantsState,
+                        projectClients : [action.payload]
+                    }
+                }else{
+                     returnState ={
+                        ...participantsState,
+                        projectClients : participantsState.projectClients.concat(action.payload)
+                    }
+                }
+
+
+
+            }
+
+                break;
+            case "REMOVE_MANAGER_INPUT_DATA": {
+
+                //A project must always have at least one manager
+                if (participantsState.projectManagers.length > 1) {
+                    return ({
+
+                        ...participantsState,
+                        projectManagers: participantsState.projectManagers.filter((manager) => (manager.username !== action.payload.username)),
+
+                    });
+                } else {
+                    return { ...participantsState }
+                }
+
+
+
+            }
+
+                break;
+            case "REMOVE_DEVELOPER_INPUT_DATA": {
+               
+                if (participantsState.projectDevelopers.length === 1) {
+
+                    let projectDevelopers: ParticipantInputData[] = getInitState().projectDevelopers;
+                    returnState = ({
+                        ...participantsState,
+                        projectDevelopers: projectDevelopers
+                    })
+                } else {
+
+                    returnState = ({
+
+                        ...participantsState,
+                        projectDevelopers: participantsState.projectDevelopers.filter((developer) => (developer.userId !== action.payload.userId)),
+
+                    });
+                }
+
+
+            }
+
+                break;
+            case "REMOVE_CLIENT_INPUT_DATA": {
+
+                if (participantsState.projectClients.length === 1) {
+                    let projectClients: ClientInputData[] = getInitState().projectClients;
+                    returnState = ({
+                        ...participantsState,
+                        projectClients: projectClients
+                    })
+                } else {
+
+                    returnState = ({
+                        ...participantsState,
+                        projectClients: participantsState.projectClients.filter((client) => (client.userId !== action.payload.userId)),
+                    });
+                }
+
+
+
+            }
+
+                break;
+
+
+
+
+            default:
+                return { ...participantsState };
+                break;
         }
-
-            break;
-        case "REMOVE_DEVELOPER_INPUT_DATA": {
-                let returnState: {
-    projectManagers: ParticipantInputData[];
-    projectDevelopers: ParticipantInputData[];
-    projectClients: ClientInputData[];
-};
-            if(participantsState.projectDevelopers.length === 1){
-
-                        let projectDevelopers: ParticipantInputData[] = [{ username: "", userType: [""], userId : -1}];
-                        returnState =({
-                            ...participantsState,
-                            projectDevelopers: projectDevelopers
-                        })  
-            }else{
-
-            returnState = ({
-              
-                ...participantsState,
-                projectDevelopers: participantsState.projectDevelopers.filter((developer) => (developer.username !== action.payload.username)),
-
-            });}
-
-
-                return returnState
-        }
-
-            break;
-        case "REMOVE_CLIENT_INPUT_DATA": {
-
-            if(participantsState.projectClients.length ===1){
-                 let projectClients: ClientInputData[] = [{ username: "", userId : -1 }];
-                        return({
-                            ...participantsState,
-                            projectClients: projectClients
-                        })    
-            }else{
-
-            return ({
-                ...participantsState,
-                projectClients:participantsState.projectClients.filter((client) => (client.username !== action.payload.username)),
-            });
-        }
-
-
-
-        }
-
-            break;
-
+        return returnState;
     
-
-
-        default:
-            return {...participantsState};
-            break;
-    }
-}   
 }
